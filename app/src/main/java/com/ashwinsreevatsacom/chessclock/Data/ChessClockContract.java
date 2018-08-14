705d309a7ecf0ab0db9314b1538d93e8deb0d1fc
Ashwin Sreevatsa <ashwin.sreevatsa@gmail.com>
2018-08-14 10:50:22 -0400

    Define contract in ChessClockContract for database

--- app/src/main/java/com/ashwinsreevatsacom/chessclock/Data/ChessClockContract.java
+++ app/src/main/java/com/ashwinsreevatsacom/chessclock/Data/ChessClockContract.java
@@ -1,4 +1,23 @@
 package com.ashwinsreevatsacom.chessclock.Data;
 
-public class ChessClockContract {
+import android.provider.BaseColumns;
+
+/**
+ * Defining database details: name, columns
+ */
+public final class ChessClockContract {
+
+    public static abstract class ChessClockEntry implements BaseColumns{
+        public static final String TABLE_NAME = "ChessClockArchive";
+
+        public static final String _ID = BaseColumns._ID; //ID for each data input
+        public static final String GAME_ID = "gameID"; //ID for each game to differentiate games in table
+        public static final String DATE = "date"; //Date of game
+        public static final String OPPONENT = "opponent"; //Opponent name
+        public static final String PIECE_COLOR = "color"; //Color of time inserted
+        public static final String TIME = "time"; //Time when button is pressed
+
+        public static final boolean PIECE_COLOR_BLACK = false;
+        public static final boolean PIECE_COLOR_WHITE = true;
+    }
 }
